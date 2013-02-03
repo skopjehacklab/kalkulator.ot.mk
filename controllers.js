26ed2abf8f079566c3be56a07f774258ad2691aa
Damjan Georgievski <gdamjan@gmail.com>
2013-02-03 17:52:48 +0100

    експортирај ги сите коефициенти во index.html
    помали промени:
    - update на angular 1.0.4
    - script-от на крај од index.html

--- controllers.js
+++ controllers.js
@@ -1,74 +1,67 @@
 function Calculator($scope) {
 
     // коефицинети
-    var ppenzisko = 0.18;
-    var pzdravstveno = 0.073;
-    var ppridones = 0.012;
-    var pzaboluvanje = 0.005;
-    var ppersonalen = 0.10;
+    $scope.k = {
+       penzisko   : 0.18,
+       zdravstveno: 0.073,
+       pridones   : 0.012,
+       boluvanje  : 0.005,
+       personalen : 0.10
+    }
 
-    var danocno_osloboduvanje = 7269;
-    var referentna_vrednost = 30595;
-    var max_osnovica_za_pridonesi = referentna_vrednost * 6;
-    var min_osnovica_za_pridonesi = Math.round(referentna_vrednost / 2);
+    $scope.danocno_osloboduvanje = 7269;
+    $scope.referentna_vrednost = 30595;
+    $scope.max_osnovica_za_pridonesi = $scope.referentna_vrednost * 6;
+    $scope.min_osnovica_za_pridonesi = Math.ceil($scope.referentna_vrednost / 2);
+    $scope.min_neto_plata = 8050;
 
     var calculate = function (bruto) {
         var osnovica_za_pridonesi = bruto;
 
-        if (bruto > max_osnovica_za_pridonesi) {
-           osnovica_za_pridonesi = max_osnovica_za_pridonesi;
-        } else if (bruto < min_osnovica_za_pridonesi) {
-           osnovica_za_pridonesi = min_osnovica_za_pridonesi;
+        if (bruto > $scope.max_osnovica_za_pridonesi) {
+           osnovica_za_pridonesi = $scope.max_osnovica_za_pridonesi;
+        } else
+        if (bruto < $scope.min_osnovica_za_pridonesi) {
+           osnovica_za_pridonesi = $scope.min_osnovica_za_pridonesi;
         }
 
-        var penzisko    = osnovica_za_pridonesi * ppenzisko;
-        var zdravstveno = osnovica_za_pridonesi * pzdravstveno;
-        var pridones    = osnovica_za_pridonesi * ppridones;
-        var zaboluvanje = osnovica_za_pridonesi * pzaboluvanje;
+        var penzisko    = osnovica_za_pridonesi * $scope.k.penzisko;
+        var zdravstveno = osnovica_za_pridonesi * $scope.k.zdravstveno;
+        var pridones    = osnovica_za_pridonesi * $scope.k.pridones;
+        var zaboluvanje = osnovica_za_pridonesi * $scope.k.boluvanje;
         var pridonesi   = penzisko + zdravstveno + pridones + zaboluvanje;
 
-        var osnovica_za_danok = bruto - danocno_osloboduvanje - pridonesi;
+        var osnovica_za_danok = bruto - $scope.danocno_osloboduvanje - pridonesi;
         osnovica_za_danok = osnovica_za_danok > 0 ? osnovica_za_danok: 0;
-
-        var personalen        = osnovica_za_danok * ppersonalen;
-        var davacki           = personalen + pridonesi;
+        var personalec        = osnovica_za_danok * $scope.k.personalen;
+        var davacki           = personalec + pridonesi;
         var neto              = bruto - davacki;
 
-        var vals = Array()
-        vals.push({ name: 'Бруто плата',                            value: bruto.toFixed(2) });
-        vals.push({ name: 'Придонес за пензискo осигурување', p: ppenzisko*100+'%',         value: penzisko.toFixed(2) });
-        vals.push({ name: 'Придонес за здравствено осигурување', p: pzdravstveno*100+'%',   value: zdravstveno.toFixed(2) });
-        vals.push({ name: 'Придонес за вработување', p: ppridones*100 + '%',                value: pridones.toFixed(2) });
-        vals.push({ name: 'Придонес за професионално заболување', p: pzaboluvanje*100+'%',  value: zaboluvanje.toFixed(2) });
-        vals.push({ name: 'Вкупно придонеси',                       value: pridonesi.toFixed(2) });
-        vals.push({ name: 'Бруто - придонеси',                      value: (bruto - pridonesi).toFixed(2) });
-        vals.push({ name: 'Даночно ослободување', p: danocno_osloboduvanje, value: danocno_osloboduvanje.toFixed(2) });
-        vals.push({ name: 'Даночна основа',                         value: osnovica_za_danok.toFixed(2) });
-        vals.push({ name: 'Персонален данок', p: ppersonalen*100+'%',       value: personalen.toFixed(2) });
-        vals.push({ name: 'Вкупно придонеси и данок',               value: davacki.toFixed(2) });
-        vals.push({ name: 'Нето плата',                             value: neto.toFixed(2) });
+        $scope.personalec = personalec;
+        $scope.osnovica_za_danok = osnovica_za_danok;
+        $scope.pridonesi = pridonesi;
 
-        return {neto: neto, vals: vals};
+        return neto;
     }
 
     var neto2bruto = function (neto) {
-        var pridonesi = ppenzisko + pzdravstveno + ppridones + pzaboluvanje;
-        var k = (1 - pridonesi) * (1 - ppersonalen);
-        var bruto = (neto - danocno_osloboduvanje / 10) / k;
+        var pridonesi = $scope.k.penzisko + $scope.k.zdravstveno +
+                        $scope.k.pridones + $scope.k.boluvanje;
+        var k = (1 - pridonesi) * (1 - $scope.k.personalen);
+        var bruto = (neto - $scope.danocno_osloboduvanje / 10) / k;
         return bruto;
     }
 
-    $scope.getBrutoVals = function() {
+    $scope.bruto_change = function() {
         var bruto = parseFloat($scope.bruto.toString());
-        var result = calculate(bruto);
-        $scope.vals =  result.vals;
-        $scope.neto =  result.neto;
+        $scope.neto = calculate(bruto);
     }
 
-    $scope.getNetoVals = function() {
-        var bruto = neto2bruto(parseFloat($scope.neto.toString()));
-        $scope.vals = calculate(bruto).vals;
-        $scope.bruto = bruto.toFixed(2);
+    $scope.neto_change = function() {
+        var neto = parseFloat($scope.neto.toString());  // cheap way to clean the input
+        var bruto = neto2bruto(neto);
+        $scope.bruto = bruto;
+        calculate(bruto);
     }
 
 }
