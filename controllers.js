b063e71b088f3ae929ad69509081722a6aeba9cd
Damjan Georgievski <gdamjan@gmail.com>
2013-02-03 18:27:19 +0100

    Math.round на пресметките кои даваат децимали
    го поправа issues/7 за сега
    можда нема да треба issues/1 да се поправа

--- controllers.js
+++ controllers.js
@@ -12,7 +12,7 @@ function Calculator($scope) {
     $scope.danocno_osloboduvanje = 7269;
     $scope.referentna_vrednost = 30595;
     $scope.max_osnovica_za_pridonesi = $scope.referentna_vrednost * 6;
-    $scope.min_osnovica_za_pridonesi = Math.ceil($scope.referentna_vrednost / 2);
+    $scope.min_osnovica_za_pridonesi = Math.round($scope.referentna_vrednost / 2);
     $scope.min_neto_plata = 8050;
 
     var calculate = function (bruto) {
@@ -25,15 +25,15 @@ function Calculator($scope) {
            osnovica_za_pridonesi = $scope.min_osnovica_za_pridonesi;
         }
 
-        var penzisko    = osnovica_za_pridonesi * $scope.k.penzisko;
-        var zdravstveno = osnovica_za_pridonesi * $scope.k.zdravstveno;
-        var pridones    = osnovica_za_pridonesi * $scope.k.pridones;
-        var zaboluvanje = osnovica_za_pridonesi * $scope.k.boluvanje;
+        var penzisko    = Math.round(osnovica_za_pridonesi * $scope.k.penzisko);
+        var zdravstveno = Math.round(osnovica_za_pridonesi * $scope.k.zdravstveno);
+        var pridones    = Math.round(osnovica_za_pridonesi * $scope.k.pridones);
+        var zaboluvanje = Math.round(osnovica_za_pridonesi * $scope.k.boluvanje);
         var pridonesi   = penzisko + zdravstveno + pridones + zaboluvanje;
 
         var osnovica_za_danok = bruto - $scope.danocno_osloboduvanje - pridonesi;
         osnovica_za_danok = osnovica_za_danok > 0 ? osnovica_za_danok: 0;
-        var personalec        = osnovica_za_danok * $scope.k.personalen;
+        var personalec        = Math.round(osnovica_za_danok * $scope.k.personalen);
         var davacki           = personalec + pridonesi;
         var neto              = bruto - davacki;
 
@@ -45,6 +45,7 @@ function Calculator($scope) {
     }
 
     var neto2bruto = function (neto) {
+        // still wrong
         var pridonesi = $scope.k.penzisko + $scope.k.zdravstveno +
                         $scope.k.pridones + $scope.k.boluvanje;
         var k = (1 - pridonesi) * (1 - $scope.k.personalen);
