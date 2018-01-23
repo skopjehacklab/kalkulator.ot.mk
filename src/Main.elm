16140d7868a33df7bb2dedeede65e4136f87a4f9
Vladan Popovic <vladanovic@gmail.com>
2018-01-23 02:26:03 +0100

    Прва worky верзија без стилови

--- src/Main.elm
+++ src/Main.elm
@@ -10,73 +10,122 @@ main =
     , update = update
     }
 
--- Ограничувања и референтни вредности
-referentna_vrednost       = 32877
-danocno_osloboduvanje     = 7531
-min_neto_plata            = 12000
-max_osnovica_za_pridonesi = referentna_vrednost * 12
-min_osnovica_za_pridonesi = referentna_vrednost / 2
-min_bruto_plata           = referentna_vrednost / 2 -- може да се случи да е хаце
+
+-- референтни вредности
+referentnaVrednost = 32877
+licnoOsloboduvanje = 7456
+
+-- ограничувања
+minNeto     = 12000
+minBruto    = 17300
+maxOsnovica = referentnaVrednost * 12
+minOsnovica = referentnaVrednost / 2
+
+-- Даноци (за сега само еден)
+type alias Danoci =
+  { pdd : Float -- персонален данок на добивка
+  }
+
+procentiDanoci : Danoci
+procentiDanoci =
+  { pdd = 0.10
+  }
+
+presmetajDanoci : Float -> Danoci -> Danoci
+presmetajDanoci osnovica d =
+  { pdd = osnovica * d.pdd
+  }
 
 -- Константни коефициенти
-type alias Davacki =
+type alias Pridonesi =
   { penzisko    : Float
   , zdravstveno : Float
   , pridones    : Float
   , boluvanje   : Float
-  , personalen  : Float
   }
 
-procenti : Davacki
-procenti =
+procentiPridonesi : Pridonesi
+procentiPridonesi =
   { penzisko    = 0.18
   , zdravstveno = 0.073
   , pridones    = 0.012
   , boluvanje   = 0.005
-  , personalen  = 0.10
   }
 
-presmetajDavacki : Float -> Davacki -> Davacki
-presmetajDavacki amount d =
-  { penzisko    = amount * d.penzisko
-  , zdravstveno = amount * d.zdravstveno
-  , pridones    = amount * d.pridones
-  , boluvanje   = amount * d.boluvanje
-  , personalen  = amount * d.personalen
+presmetajPridonesi : Float -> Pridonesi -> Pridonesi
+presmetajPridonesi bruto p =
+  { penzisko    = bruto * p.penzisko
+  , zdravstveno = bruto * p.zdravstveno
+  , pridones    = bruto * p.pridones
+  , boluvanje   = bruto * p.boluvanje
   }
 
-toList : Davacki -> List Float
-toList d =
-  [ d.penzisko
-  , d.zdravstveno
-  , d.pridones
-  , d.boluvanje
-  , d.personalen
+pridonesiToList : Pridonesi -> List Float
+pridonesiToList p =
+  [ p.penzisko
+  , p.zdravstveno
+  , p.pridones
+  , p.boluvanje
   ]
 
-suma : Davacki -> Float
-suma d =
-  List.sum (toList d)
+sumaPridonesi : Pridonesi -> Float
+sumaPridonesi p =
+  List.sum (pridonesiToList p)
 
-neto2bruto : Float -> Davacki -> Float
-neto2bruto amount procenti =
-  amount / (1 - suma procenti)
+vkupnoPridonesiProcenti : Float
+vkupnoPridonesiProcenti = sumaPridonesi procentiPridonesi
 
-bruto2neto : Float -> Davacki -> Float
-bruto2neto amount procenti =
-  amount * (1 - (suma procenti))
+
+
+bruto2neto : Float -> Pridonesi -> Model
+bruto2neto bruto procenti =
+  let
+    pridonesi = presmetajPridonesi bruto procentiPridonesi
+    vkupnoPridonesi = sumaPridonesi pridonesi
+    pddOsnovica = bruto - vkupnoPridonesi - licnoOsloboduvanje
+    danoci = presmetajDanoci pddOsnovica procentiDanoci
+    neto = bruto - vkupnoPridonesi - danoci.pdd
+  in
+    { bruto = bruto
+    , neto = neto
+    , pridonesi = pridonesi
+    , danoci = danoci
+    , pddOsnovica = pddOsnovica
+    , vkupnoDavacki = vkupnoPridonesi + danoci.pdd
+    , vkupnoPridonesi = vkupnoPridonesi
+    , brutoMinusPridonesi = bruto - vkupnoPridonesi
+    }
+
+neto2bruto : Float -> Pridonesi -> Model
+neto2bruto neto procenti =
+  let
+    pdd = ((neto - licnoOsloboduvanje) * procentiDanoci.pdd) / (1 - procentiDanoci.pdd)
+    bruto = (neto + pdd) / (1 - vkupnoPridonesiProcenti)
+  in
+    bruto2neto bruto procentiPridonesi
+
 
 type alias Model =
-  { bruto   : Float
-  , neto    : Float
-  , davacki : Davacki
+  { bruto : Float
+  , neto : Float
+  , pridonesi : Pridonesi
+  , danoci : Danoci
+  , pddOsnovica : Float
+  , vkupnoDavacki : Float
+  , vkupnoPridonesi : Float
+  , brutoMinusPridonesi : Float
   }
 
 model : Model
 model =
   { bruto = 0
-  , neto  = 0
-  , davacki = procenti
+  , neto = 0
+  , pridonesi = procentiPridonesi
+  , danoci = procentiDanoci
+  , pddOsnovica = 0
+  , vkupnoDavacki = 0
+  , vkupnoPridonesi = 0
+  , brutoMinusPridonesi = 0
   }
 
 
@@ -89,35 +138,30 @@ update : Msg -> Model -> Model
 update msg model =
   case msg of
     Bruto amount ->
-      let
-        bruto = Result.withDefault 0 (String.toFloat amount)
-        neto = bruto2neto bruto procenti
-        davacki = presmetajDavacki bruto procenti
-      in
-        { model | bruto = bruto, neto = neto, davacki = davacki }
+      bruto2neto (Result.withDefault 0 (String.toFloat amount)) procentiPridonesi
     Neto amount ->
-      let
-        neto = Result.withDefault 0 (String.toFloat amount)
-        bruto = neto2bruto neto procenti
-        davacki = presmetajDavacki bruto procenti
-      in
-        { model | bruto = bruto, neto = neto, davacki = davacki }
+      neto2bruto (Result.withDefault 0 (String.toFloat amount)) procentiPridonesi
 
 
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ type_ "text", placeholder "Бруто", onInput Bruto, value (toString model.bruto) ] []
-    , input [ type_ "text", placeholder "Нето", onInput Neto, value (toString model.neto) ] []
+    [ input [ type_ "text", placeholder "Бруто", onInput Bruto, value (toString (round model.bruto)) ] []
+    , input [ type_ "text", placeholder "Нето", onInput Neto, value (toString (round model.neto)) ] []
     , details model
     ]
 
 details : Model -> Html msg
 details model =
   div []
-    [ div [] [ text ("Пензиско: " ++ toString model.davacki.penzisko) ]
-    , div [] [ text ("Здравствено: " ++ toString model.davacki.zdravstveno) ]
-    , div [] [ text ("Придонес: " ++ toString model.davacki.pridones) ]
-    , div [] [ text ("Боледување: " ++ toString model.davacki.boluvanje) ]
-    , div [] [ text ("Персонален: " ++ toString model.davacki.personalen) ]
+    [ div [] [ text ("Придонеси за задолжително ПИО: " ++ toString (round model.pridonesi.penzisko)) ]
+    , div [] [ text ("Придонеси за задолжително здравствено осигурување: " ++ toString (round model.pridonesi.zdravstveno)) ]
+    , div [] [ text ("Придонес за вработување: " ++ toString (round model.pridonesi.pridones)) ]
+    , div [] [ text ("Дополнителен придонес за задолжително осигурување во случај повреда или професионално заболување: " ++ toString (round model.pridonesi.boluvanje)) ]
+    , div [] [ text ("Вкупно придонеси: " ++ toString (round model.vkupnoPridonesi)) ]
+    , div [] [ text ("Бруто плата намалена за придонеси: " ++ toString (round model.brutoMinusPridonesi)) ]
+    , div [] [ text ("Лично ослободување: " ++ toString (round licnoOsloboduvanje)) ]
+    , div [] [ text ("Даночна основа за пресметка на персонален данок на доход: " ++ toString (round model.pddOsnovica)) ]
+    , div [] [ text ("Вкупно придонеси и данок: " ++ toString (round model.vkupnoDavacki)) ]
+    , div [] [ text ("Нето: " ++ toString (round model.neto)) ]
     ]
